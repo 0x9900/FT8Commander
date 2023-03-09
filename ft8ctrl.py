d03567ed559f81633b4e3755e740a112fba14302
Fred C <github-fred@hidzz.com>
2023-03-08 17:58:07 -0800

    Show the country in the log

--- ft8ctrl.py
+++ ft8ctrl.py
@@ -132,8 +132,8 @@ class Sequencer:
       if not tx_status and sequence == 14:
         data = self.selector()
         if data:
-          LOG.info('Calling: %s, SNR: %d, Distance: %d, - https://www.qrz.com/db/%s',
-                   data['call'], data['snr'], data['distance'], data['call'])
+          LOG.info('Calling: %s, From: %s, SNR: %d, Distance: %d, - https://www.qrz.com/db/%s',
+                   data['call'], data['country'], data['snr'], data['distance'], data['call'])
           self.call_station(ip_from, data['call'])
           time.sleep(1)
           self.current = data['call']
