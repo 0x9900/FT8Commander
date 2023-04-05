857f076a681f5b168cbf907b45da4a30fc5f29cd
Fred C <github-fred@hidzz.com>
2023-04-05 15:51:01 -0700

    New version of DXEntity

--- DXEntity.py
+++ DXEntity.py
@@ -5,34 +5,35 @@
 # All rights reserved.
 #
 
-import io
 import csv
-import re
+import dbm.gnu as dbm
 import logging
+import marshal
+import os
+import plistlib
+import time
 
-from copy import copy
-
+from collections import defaultdict
 from importlib.resources import files
+from urllib.error import HTTPError
+from urllib.request import urlretrieve
 
+CTY_URL = "https://www.country-files.com/cty/cty.plist"
+CTY_HOME = "~/.local"
+CTY_FILE = "cty.plist"
+CTY_DB = "cty.db"
+CTY_EXPIRE = 86400 * 7          # One week
 
 class DXCCRecord:
-  __slots__ = ['prefix', 'country', 'ctn', 'continent', 'cqzone',
-               'ituzone', 'lat', 'lon', 'tz']
+  __slots__ = ['prefix', 'country', 'continent', 'cqzone', 'ituzone', 'latitude', 'longitude',
+               'gmtoffset']
 
-  def __init__(self, *args):
-    for idx, field in enumerate(DXCCRecord.__slots__):
-      if field == 'prefix':
-        prefix, *_ = args[idx].lstrip('*').split('/')
-        setattr(self, field, prefix)
-      elif field in ('cqzone', 'ituzone'):
-        setattr(self, field, int(args[idx]))
-      elif field in ('lat', 'lon', 'tz'):
-        setattr(self, field, float(args[idx]))
-      else:
-        setattr(self, field, args[idx])
-
-  def __copy__(self):
-    return type(self)(*[getattr(self, f) for f in DXCCRecord.__slots__])
+  def __init__(self, record):
+    for key, val in record.items():
+      try:
+        setattr(self, key.lower(), val)
+      except AttributeError:
+        pass
 
   def __repr__(self):
     buffer = ', '.join([f"{f}: {getattr(self, f)}" for f in DXCCRecord.__slots__])
@@ -41,46 +42,42 @@ class DXCCRecord:
 
 class DXCC:
 
-  __parser = re.compile(r'(?:=|)(?P<prefix>\w+)(?:/\w+|)(?:\((?P<cqzone>\d+)\)|)'
-                        r'(?:\[(?P<ituzone>\d+)\]|)(?:{(?P<continent>\w+)}|).*')
   def __init__(self):
-    self._map = {}
-    self._entities = set([])
-    cty = files('bigcty').joinpath('cty.csv').read_text()
-    logging.debug('Read bigcty callsign database')
-    csvfd = csv.reader(io.StringIO(cty))
-    for row in csvfd:
-      self._map.update(self.parse(row))
-    self.max_len = max(len(v) for v in self._map)
-    for record in self._map.values():
-      self._entities.add(record.country)
+    self._entities = defaultdict(set)
+    self._max_len = 0
+    self._db = os.path.join(os.path.expanduser(CTY_HOME), CTY_DB)
+    cty_file = os.path.join(os.path.expanduser(CTY_HOME), CTY_FILE)
 
-  @staticmethod
-  def parse(record):
-    dxmap = {}
-    cty = DXCCRecord(*record[:9])
-    dxmap[cty.prefix] = cty
-    extra = record[9]
-    for tag in extra.replace(';', '').split():
-      match = DXCC.__parser.match(tag)
-      if match:
-        _cty = copy(cty)
-        for key, val in match.groupdict().items():
-          if not val:
-            continue
-          setattr(_cty, key, val)
-          dxmap[_cty.prefix] = _cty
-      else:
-        logging.error('No match for %s', tag)
+    try:
+      if os.path.exists(self._db):
+        logging.info('Using DXCC cache %s', self._db)
+        with dbm.open(self._db, 'r') as cdb:
+          self._entities, self._max_len = marshal.loads(cdb['_meta_data_'])
+        return
+    except dbm.error as err:
+      logging.error(err)
 
-    return dxmap
+    logging.info('Download %s', cty_file)
+    self.load_cty(cty_file)
+    with open(cty_file, 'rb') as fdc:
+      cty_data = plistlib.load(fdc)
+    self._max_len = max(len(k) for k in cty_data)
+
+    logging.info('Create cty cache: %s', self._db)
+    with dbm.open(self._db, 'c') as cdb:
+      for key, val in cty_data.items():
+        cdb[key] = marshal.dumps(val)
+        self._entities[val['Country']].add(key)
+      cdb['_meta_data_'] = marshal.dumps([dict(self._entities), self._max_len])
 
   def lookup(self, call):
     call = call.upper()
-    prefixes = {call[:c] for c in range(self.max_len, 0, -1)}
-    for prefix in sorted(prefixes, reverse=True):
-      if prefix in self._map:
-        return self._map[prefix]
+    prefixes = list({call[:c] for c in range(self._max_len, 0, -1)})
+    prefixes.sort(key=lambda x: -len(x))
+    with dbm.open(self._db, 'r') as cdb:
+      for prefix in prefixes:
+        if prefix in cdb:
+          return DXCCRecord(marshal.loads(cdb[prefix]))
     raise KeyError(f"{call} not found")
 
   def isentity(self, country):
@@ -92,8 +89,32 @@ class DXCC:
   def entities(self):
     return self._entities
 
+  def get_entity(self, key):
+    if key in self._entities:
+      return self._entities[key]
+    raise KeyError(f'Entity {key} not found')
+
   def __str__(self):
-    return f"{self.__class__} {id(self)} ({len(self._map)} records)"
+    return f"{self.__class__} {id(self)} ({self._db})"
 
   def __repr__(self):
     return str(self)
+
+  @staticmethod
+  def load_cty(cty_file):
+    try:
+      fstat = os.stat(cty_file)
+      if fstat.st_mtime + CTY_EXPIRE < time.time():
+        return
+    except IOError:
+      pass
+
+    cty_tmp = cty_file + '.tmp'
+    try:
+      urlretrieve(CTY_URL, cty_tmp)
+      if os.path.exists(cty_file):
+        os.unlink(cty_file)
+      os.rename(cty_tmp, cty_file)
+    except HTTPError as err:
+      logging.error(err)
+      return
