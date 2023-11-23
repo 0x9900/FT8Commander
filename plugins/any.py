163038ff21bfddc21d1623e23ea4a8bfc6ce4d02
Fred C <github-fred@hidzz.com>
2023-11-22 17:06:18 -0800

    Plugins The plugin "Any" only reply to callsigns calling DX outside my continent. The plugin "CallSign" can use a regular expression and/or a list of callsigns

--- plugins/any.py
+++ plugins/any.py
@@ -9,6 +9,16 @@ from .base import CallSelector
 
 class Any(CallSelector):
 
+  def __init__(self):
+    super().__init__()
+    self.continent = getattr(self.config, 'my_continent', 'NA')
+    self.log.info('My continent %s', self.continent)
+
   def get(self, band):
-    records = super().get(band)
+    records = []
+    for record in super().get(band):
+      if all([record['extra'] == 'DX', record['continent'] == self.continent]):
+        self.log.info("Ignore %s %s %s", record['call'], record['continent'], record['extra'])
+        continue
+      records.append(record)
     return self.select_record(records)
